bb45919f43392b1cf89aaaf6421b484361acc036
Charlie Bidgood <charlesliambidgood@gmail.com>
2024-07-03 09:20:30 +1000

    code clean

--- imgmax.py
+++ imgmax.py
@@ -15,13 +15,11 @@ import re
 ############ CONFIG ############
 window_name = "ImgMax"
 icon_path = "icon.ico"
-
-
 bg_color ='#282729'
 input_color = "#414042"
 font_color = "white"
 
-# Defaults for testing
+# Regex example which is used as default
 test_regex = r"^(?P<Well>[A-Z] - [0-9]{2})\(fld (?P<Field>[0-9]{1,3}) wv (?P<Wavelength>[0-9]{1,4}) - (?P<Channel>.*) z (?P<Z>[0-9]{1,3})\).tif"
 
 def get_meta_table(path, regex):
@@ -59,10 +57,9 @@ def create_max_projections(uid_metadata, channel_ID, img_dir):
 
         for channel in unique_channels:
             output_file_name = f"{output_path}/{stack}_{channel}.tif"
-
+            
             if os.path.exists(output_file_name)==False:
                 files = stack_images[stack_images[channel_ID]==channel].index
-                
                 paths=[]
 
                 for file in files:
@@ -82,6 +79,7 @@ def create_max_projections(uid_metadata, channel_ID, img_dir):
     
                 if not os.path.exists(output_path):
                     os.makedirs(output_path)
+                
                 Image.fromarray(max_proj).save(f'{output_path}/{stack}_{channel}.tif')
                 write_console(f"{output_file_name} Created")
                 shutil.rmtree("temp/")
@@ -99,35 +97,34 @@ class chooseImages():
         window.iconbitmap(icon_path)
         window.resizable(False, False)
 
+        # Insert Logo
         logo_img = ImageTk.PhotoImage(Image.open("assets/logo.png"))
         panel = tk.Label(window, image = logo_img, bg='#282729')
         panel.grid(row=1,column=1)
 
+        # Initilise path box for image directory 
         path_box = tk.Entry(window,font=40, width=37, bg=bg_color, fg=font_color)
         path_box.grid(row=1,column=2)
-
         img_listbox = tk.Listbox(window, height = 10, width = 63, fg=font_color,
                   activestyle = 'dotbox', font = "Helvetica", bg="#414042")
-
         img_listbox.grid(row=2,column=1, columnspan = 4)
 
+        # Initilise "Browse" Button and Command
         def browsefunc():
             directory = tk.filedialog.askdirectory()
-           
             path_box.delete(0, 'end')
             path_box.insert(tk.END, directory)
             global path
             path = path_box.get()
-                    
             img_list = os.listdir(path)
             img_listbox.delete(0, 'end')
             img_listbox.insert("end", *img_list)
 
-
         browse_img = ImageTk.PhotoImage(Image.open("assets/browse.png"))
         browse_but = tk.Button(window, command=browsefunc, image=browse_img, highlightthickness=0, bd=0, bg=bg_color)
         browse_but.grid(row=1,column=3)
-    
+        
+        # Initilise "Submit" Button and Command
         def submitfunc():     
             window.destroy()
             createSets(path)
@@ -145,26 +142,19 @@ class createSets():
         window.configure(bg=bg_color)
         window.iconbitmap(icon_path)
         
-
+        # Insert Logo
         logo_img = ImageTk.PhotoImage(Image.open("assets/logo.png"))
         panel = tk.Label(window, image = logo_img, bg='#282729')
         panel.grid(row=1,column=1)
 
-
-
         # Initilise the frame and extraction table
         table_frame = tk.Frame(window, width=10)
-        
         df = pd.DataFrame([])
         table_frame.grid(row=2,column=1, columnspan=3, rowspan=4)
-
         table = pt.Table(table_frame, dataframe=df, showtoolbar=False, showstatusbar=True, width=600, height=300)
         table.show()
         
-
         # Initilise the regular expression frame and input
-
-
         regex_entry = tk.Entry(window, width = 70, bg=input_color, fg=font_color)
         regex_entry.grid(row=1,column=2)
         regex_entry.insert('end', test_regex)
@@ -184,17 +174,13 @@ class createSets():
             except: 
                 write_console("Regular Expression Error")
 
-
         check_img = ImageTk.PhotoImage(Image.open("assets/check.png"))
-        
         check_but = tk.Button(window,command=checkfunc, image=check_img, highlightthickness=0, bd=0, bg=bg_color)
         check_but.grid(row=1,column=3)
         
-     
+        # Initilise the Channel Selector
         variable = tk.StringVar(window)
         variable.set("Channel")
-
-        # Initilise the Channel Selector
         tk.Label(window, text = "Channel Identifier:", bg=bg_color, fg=font_color).grid(row=1,column=4)
 
         n = tk.StringVar() 
@@ -209,7 +195,6 @@ class createSets():
 
         # Initilise the 'RUN' Button and command
         def runfunc():
-            
             write_console(f"Starting Max Projection Run....\n")
             channel_ID = channel_select.get()
             group_by = []     
@@ -223,16 +208,14 @@ class createSets():
         run_but = tk.Button(window, text="RUN",font=40,command=runfunc)
         run_but.grid(row=5,column=4)
 
-
         # Initilise the console box
         tk.Label(window, text = "OUTPUT:", bg=bg_color, fg=font_color).grid(row=6,column=1)
 
         global console
         console = ScrolledText(window, width=100, height=15)
-            
         console.grid(row=7,column=1, columnspan=4)
         console.configure(state ='disabled') 
-
+        
         window.mainloop()
 
 chooseImages()
